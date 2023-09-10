558ec3100dff40b087d2af1e053a152b756aeabd
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-09-10 12:34:58 -0300

    add disable btn functionality

--- src/assets/Components/Current.jsx
+++ src/assets/Components/Current.jsx
@@ -7,6 +7,7 @@ const Current = () => {
 
   const [currentCity, setCurrentCity] = useState(); //To handle the API data
   const [city, setCity] = useState("Mercedes"); // Initial city to start de API
+  const [disable, setDisable] = useState(true); //Disable search btn
 
   useEffect(() => {
     const fetchCurrentWeather = async () => {
@@ -26,17 +27,33 @@ const Current = () => {
     setCity(inputValue);
   };
 
+  const disableSearchBtn = () => {
+    const inputValue = document.getElementById("input_currentCity").value;
+    console.log(inputValue.length);
+
+    if (inputValue.length >= 3) {
+      setDisable(false);
+    }
+    
+    if (inputValue.length < 3) {
+      setDisable(true);
+    }
+  };
+
   return (
     <>
-      <input type="text" id="input_currentCity" />
-      <button onClick={handleCurrentCity}>Search</button>
+      <input type="text" id="input_currentCity" onChange={disableSearchBtn} />
+      <button onClick={handleCurrentCity} disabled={disable}>
+        Search
+      </button>
 
       {currentCity && (
-          <h3>
+        <h3>
           {currentCity.location.name}, {currentCity.location.region},{" "}
           {currentCity.location.country}
         </h3>
       )}
+      {/* {!city.includes(currentCity.location.name) && <h2>incorrect</h2>} */}
     </>
   );
 };
