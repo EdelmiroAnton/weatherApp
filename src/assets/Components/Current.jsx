7acbd2aa47e50fd2801c680cd2acbf6d3202fd31
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-09-11 00:09:28 -0300

    current location working ok

--- src/assets/Components/Current.jsx
+++ src/assets/Components/Current.jsx
@@ -1,40 +1,41 @@
 import { useEffect, useState } from "react";
 
 const Current = () => {
-  //API
+  //WEATHER API
   const BASE_URL = "http://api.weatherapi.com/v1";
   const API_KEY = "db02fd76b8c04ca89a4231036231108";
 
   const [currentCity, setCurrentCity] = useState(); //To handle the API data
-  const [city, setCity] = useState("Mercedes"); // Initial city to start de API
+  const [city, setCity] = useState(""); // city variable to use in the fetch URL Weather API
   const [disable, setDisable] = useState(true); //Disable search btn
 
-  //Get coordinates from the current location
+  //Coordinates from the current location
   const [lat, setLat] = useState(); //latitude
   const [lon, setLon] = useState(); //longitude
 
+  //Effect to get information about of the user's current location
   useEffect(() => {
-    const fetchCurrentWeather = async () => {
+    const fetchCurrentCity = async () => {
       const data = await fetch(
         `${BASE_URL}/current.json?key=${API_KEY}&q=${city}`
       );
       const resp = await data.json();
       setCurrentCity(resp.location);
     };
-    fetchCurrentWeather();
+    fetchCurrentCity();
   }, [city]);
 
-  //Get latitude and longitude for geolocation. Then try to get the city with reverse geolocation
-  function success(pos) {
+  //Get latitude and longitude for geolocation
+  const success = (pos) => {
     const crd = pos.coords;
     const lat = crd.latitude;
     const lon = crd.longitude;
     setLat(lat);
     setLon(lon);
-  }
+  };
 
   const error = (err) => {
-    console.log(err);
+    alert(err.message);
   };
 
   const options = {
@@ -44,6 +45,22 @@ const Current = () => {
   };
 
   navigator.geolocation.getCurrentPosition(success, error, options);
+
+  //Reverse Geocoding. Get the city, state and country with the latitude and longitude.
+  //Geoapify API
+  const reverseGeocoding = async () => {
+    const REVERSE_API_KEY = "4cc4b204bae24c2ba3ac8a5810b5eabd";
+    const resp = await fetch(
+      `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=${REVERSE_API_KEY}`
+    );
+    const data = await resp.json();
+    setCity(
+      `${data.features[0].properties.city}, 
+      ${data.features[0].properties.state}, 
+      ${data.features[0].properties.country}`
+    );
+  };
+  reverseGeocoding();
 
   // Catch the value when the user write the city in the Input
   const searchCurrentCity = () => {
@@ -80,7 +97,7 @@ const Current = () => {
           {currentCity.name}, {currentCity.region}, {currentCity.country}
         </h3>
       ) : (
-        <h2>error</h2>
+        <h2>Buscando tu ubicación...</h2>
       )}
       <div>
         {lat} / {lon}
