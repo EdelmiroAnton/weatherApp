78ed834672badbb168a26594aefcf5d6f5a72cff
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-09-13 19:19:52 -0300

    refactor effects

--- src/assets/Components/Current.jsx
+++ src/assets/Components/Current.jsx
@@ -9,13 +9,52 @@ const Current = () => {
   const API_KEY = "db02fd76b8c04ca89a4231036231108";
 
   const [currentLocation, setCurrentLocation] = useState(); //To store the city, region and country
-  const [currentWeather, setCurrentWeather] = useState();
+  const [currentWeather, setCurrentWeather] = useState(); //To render the current weather information
   const [city, setCity] = useState(""); // city variable to use in the fetch URL Weather API
   const [disable, setDisable] = useState(true); //Disable search btn
 
   //Coordinates from the current location
   const [lat, setLat] = useState(); //latitude
   const [lon, setLon] = useState(); //longitude
+
+  //Get latitude and longitude for geolocation
+  useEffect(() => {
+    const success = (pos) => {
+      const crd = pos.coords;
+      const lat = crd.latitude;
+      const lon = crd.longitude;
+      setLat(lat);
+      setLon(lon);
+    };
+
+    const error = (err) => {
+      alert(err.message);
+    };
+
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+    //Reverse Geocoding. Get the city, state and country with the latitude and longitude.
+    //Geoapify API
+    const reverseGeocoding = async () => {
+      const REVERSE_API_KEY = "4cc4b204bae24c2ba3ac8a5810b5eabd";
+      const resp = await fetch(
+        `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=${REVERSE_API_KEY}`
+      );
+      const data = await resp.json();
+      setCity(
+        `${data.features[0].properties.city}, 
+        ${data.features[0].properties.state}, 
+        ${data.features[0].properties.country}`
+      );
+    };
+    reverseGeocoding();
+  }, [lat, lon]);
 
   //Effect to get information about of the user's current location
   useEffect(() => {
@@ -29,46 +68,6 @@ const Current = () => {
     };
     fetchCurrentCity();
   }, [city]);
-  console.log(currentLocation);
-  console.log(currentWeather);
-
-  //Get latitude and longitude for geolocation
-  const success = (pos) => {
-    const crd = pos.coords;
-    const lat = crd.latitude;
-    const lon = crd.longitude;
-    setLat(lat);
-    setLon(lon);
-  };
-
-  const error = (err) => {
-    alert(err.message);
-  };
-
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-
-  navigator.geolocation.getCurrentPosition(success, error, options);
-
-  //Reverse Geocoding. Get the city, state and country with the latitude and longitude.
-  //Geoapify API
-
-  const reverseGeocoding = async () => {
-    const REVERSE_API_KEY = "4cc4b204bae24c2ba3ac8a5810b5eabd";
-    const resp = await fetch(
-      `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=${REVERSE_API_KEY}`
-    );
-    const data = await resp.json();
-    setCity(
-      `${data.features[0].properties.city}, 
-      ${data.features[0].properties.state}, 
-      ${data.features[0].properties.country}`
-    );
-  };
-  reverseGeocoding();
 
   // Catch the value when the user write the city in the Input
   const searchCurrentCity = () => {
@@ -117,8 +116,8 @@ const Current = () => {
           <img
             src={currentWeather.condition.icon}
             alt="icon_weather_condition"
-            />
-            <h3>{currentWeather.condition.text}</h3>
+          />
+          <h3>{currentWeather.condition.text}</h3>
           <h3>
             <img src={humedad} alt="icon_humidity" className="iconHumidity" />
             {currentWeather.humidity}%
