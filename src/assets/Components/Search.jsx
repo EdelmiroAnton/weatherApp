38cc2219e88263886771cc8ddedc1d69038d924e
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-09-17 20:06:00 -0300

    save modifiacations

--- src/assets/Components/Search.jsx
+++ src/assets/Components/Search.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 
 const Search = () => {
+  //WEATHER API
   const BASE_URL = "https://api.weatherapi.com/v1";
   const API_KEY = "db02fd76b8c04ca89a4231036231108";
+
+  const inputValue = document.getElementById("input_currentCity");
 
   const [disable, setDisable] = useState(true); //Disable search btn
   const [searchedCity, setSearchedCity] = useState(); //Get the input value and use the variable to get the data from de API URL
   const [cityFromFetchData, setCityFromFetchData] = useState([]); // Store the response from the API Data
 
+  //Get and store the data from the "search.json" endpoint
   useEffect(() => {
     const fetchSearchData = async () => {
       const data = await fetch(
@@ -19,16 +23,14 @@ const Search = () => {
     fetchSearchData();
   }, [searchedCity]);
 
-  // Catch the value when the user write the city in the Input
-  let inputValue = document.getElementById("input_currentCity");
-  // let resetBtn = document.getElementById("resetBtn");
-
+  //Function to reset the API Data to initial state
   const resetSearch = () => {
     setCityFromFetchData([]);
     setDisable(true);
     inputValue.value = "";
   };
 
+  // Get the value when the user write the city in the Input
   const getUserSearch = () => {
     setSearchedCity(inputValue.value);
     if (inputValue.value === "") {
@@ -42,16 +44,10 @@ const Search = () => {
       setDisable(true);
     }
   };
-  console.log(cityFromFetchData);
 
   return (
     <>
-      <input
-        type="text"
-        id="input_currentCity"
-        onChange={getUserSearch}
-        // value={searchedCity}
-      />
+      <input type="text" id="input_currentCity" onChange={getUserSearch} />
       <button
         id="resetBtn"
         onChange={getUserSearch}
