dead323932f91dd8cc0f755d0006efbe353d55a6
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-09-18 19:45:03 -0300

    add app.css main styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import Current from "./assets/Components/Current";
 import SearchedCity from "./assets/Components/SearchedCity";
 
+import "./App.css"
 const App = () => {
   return (
-    <>
+    <div className="app_container">
       <Routes>
         <Route path="/" element={<Current />} />
         <Route path="/city/:city" element={<SearchedCity />} />
         <Route path="*" element={<Current />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
