de3e78b94afb2c80944447c271d597a3d3718b84
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-09-18 19:07:36 -0300

    add functionality to component

--- src/assets/Components/SearchedCity.jsx
+++ src/assets/Components/SearchedCity.jsx
@@ -1,11 +1,48 @@
 import { useParams } from "react-router-dom";
+import humedad from "../img/humedad.png";
+
+//Helpers
+import { getForecastDate } from "../../helpers//getForecastDate";
+import { api } from "../../helpers/weatherApi_info";
+import { useFetchData } from "../../helpers/useFetchData";
 
 const SearchedCity = () => {
   const { city } = useParams();
-
+  // const { city } = useReverseGeocoding();
+  const URL = `${api.BASEURL}/forecast.json?key=${api.APIKEY}&q=${city}&days=3`;
+  const { currentLocation, currentWeather, forecast } = useFetchData(URL);
   return (
     <>
-      <h3>{city}</h3>
+      {currentLocation && (
+        <h3>
+          {currentLocation.name}, {currentLocation.region},{" "}
+          {currentLocation.country}
+        </h3>
+      )}
+      {currentWeather && (
+        <div>
+          <h3>🌡️ {currentWeather.temp_c} °C</h3>
+          <img
+            src={currentWeather.condition.icon}
+            alt="icon_weather_condition"
+          />
+          <h3>{currentWeather.condition.text}</h3>
+          <h3>
+            <img src={humedad} alt="icon_humidity" className="iconHumidity" />
+            {currentWeather.humidity}%
+          </h3>
+          <h3>🌧️ {currentWeather.precip_in} %</h3>
+          <h3>🌬️ {currentWeather.wind_kph} km/h</h3>
+        </div>
+      )}
+      {forecast.map((el) => (
+        <>
+          <div>{getForecastDate(el.date)}</div>
+          <img src={el.day.condition.icon} alt="" />
+          <div>{el.day.mintemp_c} °C</div>
+          <div>{el.day.maxtemp_c} °C</div>
+        </>
+      ))}
     </>
   );
 };
