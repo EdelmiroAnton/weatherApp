fb5df46165476ce453fd872f7677c08b09062e5d
Edelmiro Anton <anton.edelmiro@gmail.com>
2023-10-14 21:44:27 -0300

    fix darkmode behaviour

--- src/assets/Components/DarkMode.jsx
+++ src/assets/Components/DarkMode.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
+
+//React-icons
 import { MdNightlight } from "react-icons/md";
+import { MdOutlineLightMode } from "react-icons/md";
+
 import "../Styles/darkMode.css";
+
 const DarkMode = () => {
+  const [dark, setDark] = useState(true);
 
   const handleDarkMode = () => {
     const toggleDark = document.getElementsByTagName("body")[0];
     toggleDark.classList.toggle("bodyBackground");
+
+    if (dark) {
+      document.getElementById("moon").style.display = "none";
+      document.getElementById("sun").style.display = "block";
+    } else {
+      document.getElementById("moon").style.display = "block";
+      document.getElementById("sun").style.display = "none";
+    }
+    setDark(!dark);
   };
 
   return (
     <>
       <div id="darkMode">
-        <MdNightlight size={"2em"} onClick={handleDarkMode} />
+        <div id="moon">
+          <MdNightlight size={"2em"} onClick={handleDarkMode} />
+        </div>
+        <div id="sun" className="sun">
+          <MdOutlineLightMode size={"2em"} onClick={handleDarkMode} />
+        </div>
       </div>
     </>
   );
